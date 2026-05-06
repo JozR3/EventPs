1ff5c107f68abdca4ad8134770dd470197b6ffbd
UNAJ-Tomas <165077270+UNAJ-Tomas@users.noreply.github.com>
2026-05-06 13:55:21 -0300

    Añadido Tarea Cronometrada
    La tarea cronometrada busca reservas que hayan sido creadas hace más de 5 minutos y modifica su estado a "Expired", modifica el estado del asiento de reservado a libre y crea un audit log detallando el evento. La tarea se ejecuta cada 5 segundos.
    Adicionalmente fue modificado el tiempo que dura cada reserva de 10 minutos a 5.

---
+++ App.js
@@ -0,0 +1,204 @@
+﻿async function cargarEventos(soloReservados) {
+    const contenedor = document.getElementById("contenedor");
+
+    contenedor.innerHTML = "<h2>Cargando eventos...</h2>";
+
+    try {
+        const res = await fetch("/api/events");
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+        const eventos = await res.json();
+
+        mostrarEventos(eventos, soloReservados);
+    } catch (error) {
+        contenedor.innerHTML = "<p>Error al cargar eventos</p>";
+        console.error(error);
+    }
+}
+function mostrarEventos(eventos, soloReservados) {
+    const contenedor = document.getElementById("contenedor");
+
+    contenedor.innerHTML = `
+    <div style="display:grid; grid-template-columns: repeat(auto-fill, 250px); gap:20px;">
+        ${eventos.map(e => `
+            <div onclick="verEvento(${e.id}, ${soloReservados})"
+                style="background:#222; padding:15px; border-radius:10px; cursor:pointer;">
+                <h3>${e.name}</h3>
+                <p>${new Date(e.eventDate).toLocaleDateString()}</p>
+            </div>
+        `).join("")}
+    </div>
+    `;
+}
+
+async function verEvento(eventId, soloReservados) {
+    const contenedor = document.getElementById("contenedor");
+    contenedor.innerHTML = "<h2>Cargando asientos...</h2>";
+
+    try {
+        const url = soloReservados
+            ? `/api/events/${eventId}/reservedseats`   // backend filtrado
+            : `/api/events/${eventId}/seats`;   // todos
+
+        const res = await fetch(url);
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+
+        const sectores = await res.json();
+
+        mostrarAsientos(sectores, eventId, soloReservados);
+
+    } catch (error) {
+        contenedor.innerHTML = "<p>Error al cargar asientos</p>";
+        console.error(error);
+    }
+}
+function mostrarAsientos(sectores, eventId, modo) {
+    const contenedor = document.getElementById("contenedor");
+
+    contenedor.innerHTML = sectores.map(sector => {
+        const filas = agruparPorFilas(sector.seats);
+
+        return `
+        <div style="margin-bottom:40px;">
+            <h2>${sector.sectorName}</h2>
+
+            ${filas.map(f => `
+                <div style="display:flex; align-items:center; margin-bottom:5px;">
+                    
+                    <div style="width:20px; margin-right:10px;">
+                        ${f.fila}
+                    </div>
+
+                    <div style="display:flex; gap:5px;">
+                        ${f.asientos.map(seat => `
+                            <div
+                                onclick='handleSeatClick(${JSON.stringify(seat)}, ${eventId}, ${modo})'
+                                style="
+                                    width:40px;
+                                    height:40px;
+                                    display:flex;
+                                    align-items:center;
+                                    justify-content:center;
+                                    border-radius:6px;
+                                    font-size:12px;
+                                    color:white;
+                                    cursor:${isAvailable(seat.status) ? "pointer" : "not-allowed"};
+                                    opacity:${isAvailable(seat.status) ? "1" : "0.5"};
+                                    background:${getColor(seat.status)};
+                                ">
+                                ${seat.seatNumber}
+                            </div>
+                        `).join("")}
+                    </div>
+
+                </div>
+            `).join("")}
+        </div>
+        `;
+    }).join("");
+}
+async function handleSeatClick(seat, eventId, modo) {
+    if (modo === true) {
+        // BUY
+        if (seat.status !== "Reserved") {
+            return alert("No está disponible para comprar");
+        }
+
+        try {
+            const confirmar = confirm(
+                `¿Comprar asiento ${seat.rowIdentifier}-${seat.seatNumber}?`
+            );
+            if (!confirmar) return;
+
+            const res = await fetch("/api/checkouts", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    userId: 1,
+                    seatId: seat.id
+                })
+            });
+
+            if (!res.ok) {
+                const msg = await res.text();
+                console.error(msg);
+                alert("Error: " + msg);
+                return;
+            }
+
+            alert("Compra exitosa 🎉");
+
+        } catch (error) {
+            console.error(error);
+            alert("Error de conexión");
+        }
+    }
+    else if (modo === false){
+        try {
+            const confirmar = confirm(
+                `¿Reservar asiento ${seat.rowIdentifier}-${seat.seatNumber}?`
+            );
+            if (!confirmar) return;
+
+            const res = await fetch("/api/reservation", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    userId: 1,
+                    seatId: seat.id
+                })
+            });
+
+            if (!res.ok) {
+                const msg = await res.text();
+                console.error(msg);
+                alert("Error: " + msg);
+                return;
+            }
+
+            alert("Reserva exitosa 🎉");
+
+        } catch (error) {
+            console.error(error);
+            alert("Error de conexión");
+        }
+    }
+    // 🔄 recargar asientos
+    verEvento(eventId, modo);
+}
+function agruparPorFilas(seats) {
+    const filas = {};
+
+    seats.forEach(seat => {
+        if (!filas[seat.rowIdentifier]) {
+            filas[seat.rowIdentifier] = [];
+        }
+        filas[seat.rowIdentifier].push(seat);
+    });
+
+    // Ordenar filas (A, B, C...)
+    return Object.keys(filas)
+        .sort()
+        .map(fila => ({
+            fila,
+            asientos: filas[fila].sort((a, b) => a.seatNumber - b.seatNumber)
+        }));
+}
+function isAvailable(status) {
+    return status && status.toLowerCase() === "available";
+}
+function getColor(status) {
+    switch (status.toLowerCase()) {
+        case "available":
+            return "#4CAF50"; // verde
+        case "reserved":
+            return "#FFC107"; // amarillo
+        case "occupied":
+            return "#F44336"; // rojo
+        default:
+            return "#555";
+    }
+}
---
+++ CheckoutService.cs
@@ -0,0 +1,79 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Azure.Core;
+using Domain.Entities;
+using Infraestructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventoPS.Service
+{
+    public class CheckoutService : ICheckoutService
+    {
+        private readonly EventDbContext _context;
+
+        public CheckoutService(EventDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CreateCheckoutAsync(CreateCheckoutDto request)
+        {
+            if(request == null) return false;
+            if (request.SeatId == Guid.Empty) return false;
+            // Buscar asiento
+            var seat = await _context.Seats.FindAsync(request.SeatId);
+            if (seat == null)
+                return false;
+            // Verificar usuario existe (evita violación FK)
+            var user = await _context.Users.FindAsync(request.UserId);
+            if (user == null)
+                return false;
+            // Buscar Reserva.
+            var reservation = await _context.Reservations
+                .FirstOrDefaultAsync(r => r.SeatId == request.SeatId && r.Status == "Pending");
+            if (reservation == null)
+                return false;
+
+
+            var exists = await _context.Seats
+                .AnyAsync(r => r.Id == request.SeatId && (r.Status == "Available" || r.Status == "Paid"));
+            if (exists)
+                return false;
+
+            using var transaction = await _context.Database.BeginTransactionAsync();
+            try
+            {
+                seat.Status = "Sold";
+
+                reservation.Status = "Paid";
+
+                _context.AuditLogs.Add(new Audit_Log
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = request.UserId,
+                    Action = "RESERVE_SUCCESS",
+                    EntityType = "Seat",
+                    EntityId = request.SeatId.ToString(),
+                    Details = "reserva exitosa",
+                    CreatedAt = DateTime.UtcNow
+                });
+
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+
+                return true;
+            }
+            catch (DbUpdateException)
+            {
+                // Log si deseas; por ahora devolvemos false para que el controlador informe error
+                try { await transaction.RollbackAsync(); } catch { }
+                return false;
+            }
+            catch (Exception)
+            {
+                try { await transaction.RollbackAsync(); } catch { }
+                return false;
+            }
+        }
+    }
+}
