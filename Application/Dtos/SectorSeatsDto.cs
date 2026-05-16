3f4f00942bc374338dc5e1c5408c52bd9a4b0554
Jose Ruiz <matincop2180@gmail.com>
2026-05-15 22:59:59 -0300

    Add Modification (SectorSeatsDto)

--- Application/Dtos/SectorSeatsDto.cs
+++ Application/Dtos/SectorSeatsDto.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Application.Dtos
 {
-    public class SectorSeatsDto    ///Agrupado por sector
+    public class SectorSeatsDto
     {
         public int SectorId { get; set; }
         public string SectorName { get; set; }
+        public decimal Price { get; set; } 
         public List<SeatDto> Seats { get; set; }
     }
 }
